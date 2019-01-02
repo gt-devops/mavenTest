67d4e7cef00c660a917f97255b72afb7ce2cf3ff
gtyagi2018 <40911170+gtyagi2018@users.noreply.github.com>
2019-01-03 01:39:00 +0530

    Update App.java
    Comment

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,9 @@ public class App
 
     private static String message = "Hello World!";
 
-    public App() {}
+    public App() {
+    // Comment
+    }
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
